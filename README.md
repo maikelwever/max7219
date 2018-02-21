034a268737fa3ebe8a46be99ab76be88b29619c5
Maikel Wever <maikelwever@gmail.com>
2018-02-21 20:13:16 +0100

    Add support for addressing and chaining

--- README.md
+++ README.md
@@ -8,11 +8,11 @@
 
 - Powering on/off the MAX chip
 - Basic commands for setting LEDs on/off.
+- Chaining support (max 8 devices)
 
 ## TODO
 
 - [ ] Using hardware SPI
-- [ ] Implementing chaining support
 
 ## Example
 
@@ -21,25 +21,22 @@ Here is a simple example for using the MAX7219 on a stm32f103xx device with stm3
 #![deny(unsafe_code)]
 #![no_std]
 
-extern crate cortex_m;
 extern crate stm32f103xx_hal as hal;
 extern crate max7219;
 
 use hal::stm32f103xx;
 use hal::prelude::*;
 
-use max7219::{MAX7219, Command};
+use max7219::{MAX7219, Command, DecodeMode};
 
 
 fn main() {
     let dp = stm32f103xx::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
     let _clocks = rcc.cfgr
         .sysclk(64.mhz())
@@ -50,18 +47,25 @@ fn main() {
     let max_clk = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
     let max_cs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
 
-    let mut max7219 = MAX7219::new(max_data, max_cs, max_clk);
+    let number_of_devices: u8 = 1;
+    let mut max7219 = MAX7219::new(number_of_devices, max_data, max_cs, max_clk);
     max7219.power_on();
-    max7219.set_intensity(8);
+    max7219.set_intensity(0, 8);
 
-    max7219.write_raw(0x01, 0x01);
-    max7219.write_raw(0x02, 0x02);
-    max7219.write_raw(0x03, 0x03);
-    max7219.write_raw(0x04, 0x04);
-    max7219.write_raw(0x05, 0x05);
-    max7219.write_raw(0x06, 0x06);
-    max7219.write_raw(0x07, 0x07);
-    max7219.write_raw(0x08, 0x08);
+    // For a 7-segment display, optionally set a decode mode:
+    // max7219.set_decode_mode(DecodeMode::CodeBDigits7_0);
+    // You can add a dot to any number with an OR operation:
+    // max7219.write_raw(0x01, 0x01 | 0x80);
+    // Numbers 0-9 are written using their raw values. 0x0F is empty
+
+    max7219.write_raw(0, 0x01, 1);
+    max7219.write_raw(0, 0x02, 0x02);
+    max7219.write_raw(0, 0x03, 3);
+    max7219.write_data(0, Command::Digit4, 0x04);
+    max7219.write_raw(0, 0x05, 0x05);
+    max7219.write_raw(0, 0x06, 0x06 | 0x80);
+    max7219.write_raw(0, 0x07, 0x05);
+    max7219.write_raw(0, 0x08, 0x8F);
 }
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,13 @@ use embedded_hal::digital::OutputPin;
 pub enum Command {
     Noop = 0x00,
     Digit0 = 0x01,
+    Digit1 = 0x02,
+    Digit2 = 0x03,
+    Digit3 = 0x04,
+    Digit4 = 0x05,
+    Digit5 = 0x06,
+    Digit6 = 0x07,
+    Digit7 = 0x08,
     DecodeMode = 0x09,
     Intensity = 0x0A,
     ScanLimit = 0x0B,
@@ -24,62 +31,97 @@ pub enum Command {
     DisplayTest = 0x0F
 }
 
+pub enum DecodeMode {
+    NoDecode = 0x00,
+    CodeBDigit0 = 0x01,
+    CodeBDigits3_0 = 0x0F,
+    CodeBDigits7_0 = 0xFF
+}
+
 
 pub struct MAX7219<DATA, CS, CLK> {
     data: DATA,
     cs: CS,
-    clk: CLK
+    clk: CLK,
+    devices: u8,
+    buffer: [u8; 8]
 }
 
 impl<DATA, CS, CLK> MAX7219<DATA, CS, CLK>
     where DATA: OutputPin, CS: OutputPin, CLK: OutputPin {
-        pub fn new(data: DATA, cs: CS, clk: CLK) -> Self {
+        pub fn new(devices: u8, data: DATA, cs: CS, clk: CLK) -> Self {
+
+            let mut num_devices = devices;
+            if num_devices > 8 {
+                num_devices = 8;
+            }
+
             let mut max7219 = MAX7219 {
-                data, cs, clk
+                data, cs, clk, 
+                devices: num_devices, 
+                buffer: [0; 8]
             };
 
             max7219.init();
-            return max7219;
+            max7219
         }
 
         pub fn init(&mut self) {
-            self.cs.set_high();
-            self.write_command(Command::DisplayTest);
-            self.write_data(Command::ScanLimit, 0x07);
-            self.write_command(Command::DecodeMode);
-            self.clear_display();
+            for i in 0..self.devices {
+                self.write_command(i, Command::DisplayTest);
+                self.write_data(i, Command::ScanLimit, 0x07);
+                self.set_decode_mode(i, DecodeMode::NoDecode);
+                self.clear_display(i);
+            }
             self.power_off();
         }
 
+        pub fn set_decode_mode(&mut self, addr: u8, mode: DecodeMode) {
+            self.write_data(addr, Command::DecodeMode, mode as u8);
+        }
+
         pub fn power_on(&mut self) {
-            self.write_data(Command::Power, 0x01);
+            for i in 0..self.devices {
+                self.write_data(i, Command::Power, 0x01);
+            }
         }
 
         pub fn power_off(&mut self) {
-            self.write_data(Command::Power, 0x00);
+            for i in 0..self.devices {
+                self.write_data(i, Command::Power, 0x00);
+            }
         }
 
-        pub fn write_command(&mut self, command: Command) {
-            self.write_data(command, 0x00);
+        pub fn write_command(&mut self, addr: u8, command: Command) {
+            self.write_data(addr, command, 0x00);
         }
 
-        pub fn write_data(&mut self, command: Command, data: u8) {
-            self.write_raw(command as u8, data);
+        pub fn write_data(&mut self, addr: u8, command: Command, data: u8) {
+            self.write_raw(addr, command as u8, data);
         }
 
-        pub fn write_raw(&mut self, header: u8, data: u8) {
-            self.shift_out(header);
-            self.shift_out(data);
-            self.latch();
+        fn empty_buffer(&mut self) {
+            self.buffer = [0; 8];
         }
 
-        pub fn latch(&mut self) {
-            self.cs.set_high();
+        pub fn write_raw(&mut self, addr: u8, header: u8, data: u8) {
+            let offset = addr * 2;
+            let max_bytes = self.devices * 2;
+            self.empty_buffer();
+
+            self.buffer[offset as usize] = header;
+            self.buffer[offset as usize + 1] = data;
+
             self.cs.set_low();
+            for i in 0..max_bytes {
+                let buffer_data = self.buffer[i as usize];
+                self.shift_out(buffer_data);
+            }
+            self.cs.set_high();
         }
 
-        pub fn set_intensity(&mut self, intensity: u8) {
-            self.write_data(Command::Intensity, intensity);
+        pub fn set_intensity(&mut self, addr: u8, intensity: u8) {
+            self.write_data(addr, Command::Intensity, intensity);
         }
 
         fn shift_out(&mut self, value: u8) {
@@ -96,17 +138,17 @@ impl<DATA, CS, CLK> MAX7219<DATA, CS, CLK>
 
         }
 
-        pub fn clear_display(&mut self) {
+        pub fn clear_display(&mut self, addr: u8) {
             for i in 1..9 {
-                self.write_raw(i, 0x00);
+                self.write_raw(addr, i, 0x00);
             }
         }
 
-        pub fn test(&mut self, is_on: bool) {
+        pub fn test(&mut self, addr: u8, is_on: bool) {
             if is_on {
-                self.write_raw(0x01, 0x01);
+                self.write_raw(addr, 0x01, 0x01);
             } else {
-                self.write_raw(0x01, 0x00);
+                self.write_raw(addr, 0x01, 0x00);
             }
         }
     }
